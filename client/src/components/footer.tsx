b6a2f5aaf4cbe3e102e94460fc661bb79bd37e16
Vincent <534002646@qq.com>
2024-07-20 02:38:38 +0800

    update footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -50,7 +50,7 @@ function Footer() {
                 </div>
                 {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
                 <p className='text-sm text-neutral-500 font-normal link-line text-center'>
-                    <img className="w-[16px] h-[16px]" src="https://icp.gov.moe/images/ico64.png">
+                    {<div dangerouslySetInnerHTML={{ __html: '<img className="w-[16px] h-[16px]" src="https://icp.gov.moe/images/ico64.png">' }} />}
                     <a className='hover:underline' href="https://icp.gov.moe/?keyword=20245001" target="_blank">萌ICP备20245001号</a>
                     <br/>
                     <span>
